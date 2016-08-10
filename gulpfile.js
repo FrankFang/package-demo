681cff488bd20a4f23e5ffcc90f09689591dc21f
frankfang <frankfang1990@gmail.com>
2016-08-10 20:58:21 +0800

    CHG: before gulp less

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+
+});
+
+
+var less = require('gulp-less');
+var path = require('path');
+
+gulp.task('less', function () {
+  return gulp.src('./src/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./src/'));
+});
